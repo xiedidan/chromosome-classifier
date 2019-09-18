70fb22740a892fafcf44c3ca9252c061bcc1395d
Xie Di <xiedidan@yeah.net>
2019-09-18 17:09:11 +0800

    update train script

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
-CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.001 --end_epoch 200 --batch_size 12 --root /home/voyager/data/chromosome/ 
+CUDA_VISIBLE_DEVICES=0 python train.py --lr 0.0001 --end_epoch 200 --batch_size 12 --root /home/voyager/data/chromosome/ 
 
