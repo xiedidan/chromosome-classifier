421cbf2db567d88e07e95a67184f027056d9a6e0
Xie Di <xiedidan@yeah.net>
2019-09-20 17:48:17 +0800

    dev commit

--- transforms.py
+++ transforms.py
@@ -1,7 +1,121 @@
+import os
+
 import torchvision.transforms as transforms
 import numpy as np
 import cv2
 import PIL
+from sklearn.cluster import DBSCAN
+
+def binaryzation(img, binary_threshold):
+    _, b_img = cv2.threshold(img, binary_threshold, 255, cv2.THRESH_BINARY_INV)
+    
+    return b_img
+
+def opening(img, kernel_size):
+    kernel = np.ones((kernel_size, kernel_size), np.uint8)
+    opening_img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+    
+    return opening_img
+
+def denoise(img, area_threshold):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    areas = []
+    for contour in contours:
+        areas.append(cv2.contourArea(contour))
+    
+    filtered_indexes = []
+    filtered_areas = []
+    for i, area in enumerate(areas):
+        if area > area_threshold:
+            filtered_indexes.append(i)
+            filtered_areas.append(area)
+    
+    return filtered_indexes, filtered_areas, contours
+
+def circularity(contour):
+    area = cv2.contourArea(contour)
+    center, r = cv2.minEnclosingCircle(contour)
+    
+    ratio = np.min((1., area / (r * r * np.pi)))
+
+    return ratio
+
+def cell_filter(indexes, areas, contours, circularity_threshold, cell_threshold):
+    c_indexes = []
+
+    for index, area in zip(indexes, areas):
+        if circularity(contours[index]) < circularity_threshold and area < cell_threshold:
+            c_indexes.append(index)
+
+    return c_indexes
+
+def dbscan_filter(indexes, contours, eps, min_samples):
+    d_indexes = []
+
+    # find out contour centers
+    centers = []
+    for contour in contours:
+        center, r = cv2.minEnclosingCircle(contour)
+        centers.append(center)
+        
+    # clustering
+    dataset = np.array(centers)
+    pred = DBSCAN(eps=eps, min_samples=min_samples).fit_predict(dataset)
+    
+    # filter
+    for index, p in zip(indexes, pred):
+        if p != -1:
+            d_indexes.append(index)
+
+    return d_indexes
+
+class AutoMask:
+    def __init__(
+        self,
+        binary_threshold=225,
+        noise_threshold=200,
+        opening_kernel_size=5,
+        circularity_threshold=0.7,
+        cell_threshold=7500,
+        dbscan_eps=200,
+        dbscan_samples=3
+        ):
+        self.binary_threshold = binary_threshold
+        self.noise_threshold = noise_threshold
+        self.opening_kernel_size = opening_kernel_size
+        self.circularity_threshold = circularity_threshold
+        self.cell_threshold = cell_threshold
+        self.dbscan_eps = dbscan_eps
+        self.dbscan_samples = dbscan_samples
+
+    def __call__(self, img):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        b_img = binaryzation(gray, self.binary_threshold)
+        o_img = opening(b_img, self.opening_kernel_size)
+        indexes, areas, contours = denoise(o_img, self.noise_threshold)
+        indexes = cell_filter(indexes, areas, contours, self.circularity_threshold, self.cell_threshold)
+        indexes = dbscan_filter(indexes, contours, self.dbscan_eps, self.dbscan_samples)
+
+        # create mask
+        mask = np.zeros_like(gray)
+        for i in indexes:
+            cv2.drawContours(mask, contours, i, 255, -1)
+
+        # apply mask
+        new_img = np.full_like(gray, 255)
+        np.copyto(new_img, gray, where=(mask>127))
+
+        return new_img
+
+# convert PIL.Image to ndarray
+class ToNumpy(object):
+    def __init__(self):
+        pass
+    
+    def __call__(self, img):
+        return np.asarray(img)
 
 class Invert(object):
     def __init__(self):
