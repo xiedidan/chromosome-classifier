6d65067cb91ce95e673dddd65576d31dad666a94
Xie Di <xiedidan@yeah.net>
2020-01-10 12:07:25 +0800

    round 2 train 1 script

---
+++ train_round-2_1.sh
@@ -0,0 +1,1 @@
+python triplet-train.py --batch_size 256 --img_size 256 --device cuda:0 --data_root /mnt/nvme/data/chromosome --img_path neg-chunk --anno_paths anno_round-1.csv anno_round-2.csv --lr 1e-5 --weight_decay 1e-5 --margin 1. --scheduler_step 800 --scheduler_gamma 0.1 --round_id 2 --train_id 1 --epoches 1000 --amp_opt O1 --ohem Semihard
