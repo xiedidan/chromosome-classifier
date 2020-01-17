24a34944f820aa66011cf27fcf894b4e36ffcd07
Xie Di <xiedidan@yeah.net>
2020-01-17 15:34:34 +0800

    more script for weight decay

---
+++ train_round-2_14.sh
@@ -0,0 +1,1 @@
+python triplet-train.py --batch_size 128 --img_size 256 --device cuda:0 --data_root /home/xd/data/chromosome --img_path neg-chunk --anno_paths anno_round-1.csv anno_round-2.csv --lr 1e-5 --weight_decay 1e-2 --margin 1. --scheduler_step 800 --scheduler_gamma 0.1 --round_id 2 --train_id 14 --epoches 1200 --amp_opt O0 --ohem Random
