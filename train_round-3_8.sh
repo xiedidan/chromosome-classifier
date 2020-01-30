5ef824ee1ad34a8ee513add2398e7963b0d67fbc
Xie Di <xiedidan@yeah.net>
2020-01-30 17:51:34 +0800

    more training rounds

---
+++ train_round-3_8.sh
@@ -0,0 +1,1 @@
+python arcface-train.py --checkpoint models/EmbeddingNet-3-5.pth --batch_size 128 --img_size 256 --device cuda:0 --data_root /home/xd/data/chromosome --img_path neg-chunk --anno_paths anno_round-1.csv anno_round-2.csv --lr 1e-5 --weight_decay 1e-5 --metric arc_margin --focal --scheduler_step 800 --scheduler_gamma 0.1 --round_id 3 --train_id 8 --epoches 1200 --amp_opt O0
